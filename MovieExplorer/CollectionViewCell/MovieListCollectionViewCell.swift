0b580033f289e22a09cf7f60c77a1731d78d1dd5
Md.Omar Faruq <43860406+faruqiAhmed@users.noreply.github.com>
2024-02-15 21:45:07 +0600

    Created MovieList CollectionView Cell XIB File

---
+++ MovieExplorer/CollectionViewCell/MovieListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListCollectionViewCell.swift
+//  MovieExplorer
+//
+//  Created by Md Omar Faruq on 15/2/24.
+//
+
+import UIKit
+
+class MovieListCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
