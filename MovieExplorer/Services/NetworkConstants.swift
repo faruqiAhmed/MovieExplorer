c9b536f94c0a75ddae31480e81a86e45fdba9dce
Md.Omar Faruq <43860406+faruqiAhmed@users.noreply.github.com>
2024-02-16 00:29:11 +0600

    Created NetworkConstants and Api caller

---
+++ MovieExplorer/Services/NetworkConstants.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkConstants.swift
+//  MovieExplorer
+//
+//  Created by Md Omar Faruq on 16/2/24.
+//
+
+import Foundation
+class NetworkConstants {
+    public static var shared: NetworkConstants = NetworkConstants()
+    
+    public var apiKey: String {
+        get {
+         //   https://www.themoviedb.org/
+            //Put your own API key here
+            return "8cb15aa4aa8011e577af1a6059db4243"
+        }
+    }
+    
+    public var serverAddress: String {
+        get {
+            return "https://api.themoviedb.org/3/"
+        }
+    }
+    
+    public var imageServerAddress: String {
+        get {
+            return "https://image.tmdb.org/t/p/w500/"
+        }
+    }
+    
+}
