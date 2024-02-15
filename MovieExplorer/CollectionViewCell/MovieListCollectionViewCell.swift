22686f9f7ba6ac773482349dd82ca0df6fbda148
Md.Omar Faruq <43860406+faruqiAhmed@users.noreply.github.com>
2024-02-15 22:55:58 +0600

    Movie List Page Design  Collection View Methods  catered

--- MovieExplorer/CollectionViewCell/MovieListCollectionViewCell.swift
+++ MovieExplorer/CollectionViewCell/MovieListCollectionViewCell.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class MovieListCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet weak var imgeView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- MovieExplorer/ViewController/MovieListViewController.swift
+++ MovieExplorer/ViewController/MovieListViewController.swift
@@ -7,23 +7,42 @@
 
 import UIKit
 
-class MovieListViewController: UIViewController {
-
+class MovieListViewController: UIViewController ,UICollectionViewDataSource,UICollectionViewDelegate  {
+   
+    var cell = MovieListCollectionViewCell()
+    @IBOutlet weak var movieListColl: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.movieListColl.dataSource=self
+        self.movieListColl.delegate=self
+        self.movieListColl.register(UINib(nibName: "MovieListCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "MovieListCollectionViewCell")
+        let cellSize = CGSize(width:200 , height:182)
+        
+        self.navigationController?.isNavigationBarHidden = true
+        
+
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal //.horizontal
+        layout.itemSize = cellSize
+//        layout.sectionInset = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+//        layout.minimumLineSpacing = 1.0
+//        layout.minimumInteritemSpacing = 1.0
+        self.movieListColl.setCollectionViewLayout(layout, animated: true)
+        self.movieListColl.isScrollEnabled=true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        cell = movieListColl.dequeueReusableCell(withReuseIdentifier: "MovieListCollectionViewCell", for: indexPath) as! MovieListCollectionViewCell
+        
+        return cell
+       
+    }
+   
 
 }
